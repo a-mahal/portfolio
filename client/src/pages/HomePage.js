61c534be549775dcfde7814b1092ed9266cf3289
a-mahal <arjunmahal5@gmail.com>
2025-08-16 15:11:06 -0600

    added navbar

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+import { Container, Divider, Link } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+
+
+//const config = require('../config.json');
+
+export default function HomePage() {
+
+  return (
+    <Container>
+      <h2>Check out your song of the day</h2>
+    </Container>
+  );
+};
